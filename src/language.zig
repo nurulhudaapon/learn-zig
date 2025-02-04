83e49825c8754208c45a1142992f298f774928a2
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-02-04 07:53:45 +0000

    progress: conditionals and loops

--- src/language.zig
+++ src/language.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const util = @import("util.zig");
+const print = std.debug.print;
 
 pub fn main() void {
     // Assignment
@@ -7,11 +8,51 @@ pub fn main() void {
     const person = "Nurul";
     var age: u8 = 23;
     age = age + 1;
-    std.debug.print("{s} is {} years old!\n\n\n", .{ person, age });
+    print("{s} is {} years old!", .{ person, age });
 
     // Arrays
     util.title("Arrays");
-    const skills = [_]u8{ 'Z', 'i', 'g' };
+    const skills = [_]u8{ 'Z', 'i', 'g' }; // array of infered size chars (integer under the hood)
     const commits = [_]u8{ 1, 2, 3 };
-    std.debug.print("Skills: {s}, Commits: {d}\n", .{ skills, commits });
+    const degrees = [_][]const u8{ "High School", "Grad School" }; // inferrd sized array of unknown sized string
+    print("Skills: {s}\nCommits: {d}\nDegrees: {s}", .{ skills, commits, degrees });
+
+    // If Expressions
+    util.title("If Expressions");
+    if (age > 18) print("{s} is now adult!\n", .{person});
+    const is_adult = if (age >= 18) true else false;
+    if (is_adult) print("Yes, {s} is adult!\n", .{person});
+    age = age - 10;
+    if (age > 18) {
+        print("{s} is now adult!\n", .{person});
+    } else {
+        print("{s} is not an adult!", .{person});
+    }
+
+    // While loops
+    util.title("While Loops");
+    const year_of_birth: u16 = 2001;
+    var years_old: u8 = 0;
+    const current_ts = std.time.timestamp(); // Seconds from relative to 1970-01-01
+    const year = @divFloor(current_ts, 365 * std.time.s_per_day) + 1970;
+
+    while (years_old <= 50) {
+        const current_year = year_of_birth + years_old;
+        const is_future = current_year > year;
+        const verb = if (is_future) "will be" else "was";
+
+        if (is_future) print("In {} {s} {s} {} years old\n", .{ current_year, person, verb, years_old });
+        years_old = years_old + 1;
+    }
+
+    // For loops
+    util.title("For loops");
+    for (degrees) |degree| {
+        print("{s}, ", .{degree});
+    }
+    print("\n", .{});
+    for (degrees, 0..) |degree, index| {
+        print("{s}", .{degree});
+        if (index == degrees.len - 1) print("\n", .{}) else print(", ", .{});
+    }
 }
--- src/util.zig
+++ src/util.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 
 // Functions
 pub fn title(comptime fmt: []const u8) void {
-    std.debug.print("\x1b[1;32m➜ {s}:\x1b[0m\n", .{fmt});
+    std.debug.print("\n\n\x1b[1;32m➜ {s}:\x1b[0m\n", .{fmt});
 }
